00e97aa475389a46572aafe9e0a06804214dd2a9
dnoseda <damian.noseda@mercadolibre.com>
2010-09-23 11:27:51 -0300

    solo se puede probar cantidad por web

--- test/webtest/webtfail/ItemWebTests.groovy
+++ test/webtest/webtfail/ItemWebTests.groovy
@@ -11,13 +11,19 @@ class ItemWebTests extends grails.util.WebTest {
 	
 	void testSomething() {
 		String descriptionValue = "cualquier cosa"
-		int itemCant = Item.findAllByDescription(descriptionValue).size()
+		int itemCant = Item.count()
 		invoke '/item/list'
+		storeXPath(xpath:'count(//table//tbody//tr)',property:"uri", description:'cantidad previa')
 		clickElement(xpath:"//a[@class='create']")
 		setInputField(name:'description', value:descriptionValue)
 		clickElement(xpath:"//input[@id='create']")
 		verifyXPath(xpath:"//div[@class='message']/text()='Item 1 created'")
-		int itemAfterCant = Item.findAllByDescription(descriptionValue).size()
+		int itemAfterCant = Item.count()
+		
+		invoke '/item/list'
+		storeXPath(xpath:'count(//table//tbody//tr)',property:"uri", description:'cantidad luego')
+		verifyXPath(xpath:'count(//table//tbody//tr)=1')
+		
 		if(itemCant==itemAfterCant){
 			not{
 				invoke url:"/", description:"itemCant=${itemCant} itemAfterCant: ${itemAfterCant}"
